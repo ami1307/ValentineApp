30d0628ffe89ca7bfbb322617bd5cf8e4724ef00
ami1307 <aminirmal1307@gmail.com>
2026-02-02 15:39:12 +0530

    Add files via upload

---
+++ src/test/java/com/example/valentine/ValentineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.valentine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValentineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
